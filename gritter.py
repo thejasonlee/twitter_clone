ddb3b0e8c138235f254cb095749dd68bdb77958f
Aaron Shyuu <aaronshyuu@gmail.com>
2020-10-08 18:29:28 -0700

    Improved user model

--- gritter.py
+++ gritter.py
@@ -10,9 +10,14 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(50), unique = True, nullable = False)
-    password = db.Column(db.String(50), unique = True, nullable = False)
+    password = db.Column(db.String(50), nullable = False)
     email = db.Column(db.String(100), unique = True, nullable = False)
-    when_created = db.Column(db.DateTime, default = datetime.utcnow)
+    profile_photo = db.Column(db.String(20), nullable = False, default = 'default.jpg')
+    when_joined = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
 
 
 @app.route('/')
