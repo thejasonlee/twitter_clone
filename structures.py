e0edab92298b0cdf009a8273e1b10dacdb1e2cff
Francis Kim <francis.kim@colorado.edu>
2020-10-11 22:54:33 +0900

    1. Created user_home route at user_home.html with post feature 2. Created UserPost form 3. Added db Post model 3. Added db migrate 4. Extended layout to all pages 5. Minor changes to syntax and names

--- structures.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-
-class SignUpForm(FlaskForm):
-    username = StringField('Username')
-    email = StringField('Email')
-    password = PasswordField('Password')
-    submit = SubmitField('Sign Up :D')
