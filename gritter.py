2ed6dd742ed066c38a9d8405d771e3e208028589
JLW <jweidner@gmail.com>
2020-10-19 10:55:03 +1000

    Configuring manage.py file for production.

--- gritter.py
+++ gritter.py
@@ -1,47 +1,12 @@
 from flask import Flask, render_template, url_for, redirect, flash, send_from_directory, request
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-import os
 from forms import SignUpForm, UserPost
-from flask_migrate import Migrate
+from manage import User, Post
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'blahblahblah'
 
-'''Database configuration
-If the environment variable 'DATABASE_URL' is defined, then use it.
-Otherwise, default to the sqlite database.
-'''
-_default_sqlite_db = "userDatabase.db"
-SQLALCHEMY_DATABASE_URI = os.environ.get(
-    "DATABASE_URL", f"sqlite:///{_default_sqlite_db}"
-)
-
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    profile_photo = db.Column(db.String(20), nullable=False, default='default.jpg')
-    when_joined = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    #posts = db.relationship('Post', backref='author', lazy='dynamic')
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    content = db.Column(db.Text)
-    #user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        return f"Post('{self.timestamp}', '{self.content}')"
-
 
 @app.route('/')
 def home():
---
+++ manage.py
@@ -0,0 +1,57 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from datetime import date
+import os
+
+
+app = Flask(__name__)
+
+'''Database configuration
+If the environment variable 'DATABASE_URL' is defined, then use it.
+Otherwise, default to the sqlite database.
+'''
+_default_sqlite_db = "userDatabase.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+        "DATABASE_URL", f"sqlite:///{_default_sqlite_db}"
+    )
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+'''
+        ***************************
+        **** MODEL DEFINITIONS ****
+        ***************************
+'''
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    profile_photo = db.Column(db.String(20), nullable=False, default='default.jpg')
+    when_joined = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    #posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    content = db.Column(db.Text)
+    #user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f"Post('{self.timestamp}', '{self.content}')"
+
+
+
+if __name__ == '__main__':
+    manager.run()
+
