8c5a4d43e9f69bfe3274a8dd85a6cfafe58a0b58
Aaron Shyuu <aaronshyuu@gmail.com>
2020-10-24 23:46:19 -0700

    add signIn form

--- forms.py
+++ forms.py
@@ -8,6 +8,11 @@ class SignUpForm(FlaskForm):
     password = PasswordField('Password', validators=[InputRequired('A password is required!'), Length(min=5, max=20, message='Must be between 5 and 20 characters')])
     submit = SubmitField('Sign Up :D')
 
+class SignInForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In :P')
+
 class UserPost(FlaskForm):
     content = TextAreaField('Message', validators=[DataRequired()])
     submit = SubmitField('Post')
