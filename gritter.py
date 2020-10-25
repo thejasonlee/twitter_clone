7e57ec1c7e79a7fa3e2c3c6e9affc2013672930b
Aaron Shyuu <aaronshyuu@gmail.com>
2020-10-24 23:47:55 -0700

    fix signUP validation

--- gritter.py
+++ gritter.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, redirect, flash, send_from_di
 from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_migrate import Migrate
-from forms import SignUpForm, UserPost
+from forms import SignUpForm, SignInForm, UserPost
 from models import User, Post, db
 
 
@@ -46,25 +46,27 @@ def home():
     return render_template('home.html')
 
 
-
-@app.route('/signin')
-def signin():
-    return render_template('signIn.html')
-
-
-
 @app.route('/signup', methods = ['GET', 'POST'])
 def signup():
     form = SignUpForm()
-    if form.is_submitted():
-        getback = request.form
+   # if form.is_submitted():
+    #    getback = request.form
         # line here to save the form data into the database
-        return render_template('user.html', getback = getback)
+     #   return render_template('user.html', getback = getback)
 
     if form.validate_on_submit():
+        user = User(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
         return render_template('user.html')
     return render_template('signUp.html', form = form)
 
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+   # form = SignInForm()
+    
+    return render_template('signIn.html')
 
 
 @app.route('/user/home', methods=['GET', 'POST'])
