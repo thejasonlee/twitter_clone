e9a66fa8ebb12521d44616a45484d5f726126d95
Aaron Shyuu <aaronshyuu@gmail.com>
2020-11-08 15:00:51 -0800

    update form

--- forms.py
+++ forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
-from wtforms.validators import InputRequired, DataRequired, Length
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import InputRequired, DataRequired, Length, EqualTo, Email
 
 class SignUpForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired('A username is required!'), Length(min=5, max=20, message='Must be between 5 and 20 characters')])
-    email = StringField('Email', validators=[InputRequired('An email is required!'), Length(min=5, max=50, message='Must be between 5 and 50 characters')])
-    password = PasswordField('Password', validators=[InputRequired('A password is required!'), Length(min=5, max=20, message='Must be between 5 and 20 characters')])
-    submit = SubmitField('Sign Up :D')
+    username= StringField('Username',validators=[InputRequired(), Length(min=5, max=30, message='Must be between 5 and 30 characters')])
+    email= StringField('Email',validators=[InputRequired(), Email()])
+    password= PasswordField('Password', validators=[InputRequired(), Length(min=5, max=30, message='Must be between 5 and 30 characters')])
+    confirmPassword = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password'), Length(min=5, max=30, message='Must be between 5 and 30 characters')])
+    submit= SubmitField("Sign Up :D")
 
 class SignInForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
