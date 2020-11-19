6f6fe7b2a381fcc7977432d8c50053d0b121ab25
Aaron Shyuu <aaronshyuu@gmail.com>
2020-11-18 19:15:36 -0800

    fix migration issue

--- project/models.py
+++ project/models.py
@@ -38,7 +38,3 @@ class Like(db.Model):
         return f"Post('{self.content}')"
 
 
-followers = db.Table('followers',
-    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-)
