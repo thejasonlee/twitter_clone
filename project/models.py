0a49484936af92fad5f31dbf6d6f98236a3a1bd6
Aaron Shyuu <aaronshyuu@gmail.com>
2020-11-18 18:01:10 -0800

    establish one to many rel

--- project/models.py
+++ project/models.py
@@ -13,6 +13,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
+    posts = db.relationship('Post', backref ='author', lazy='dynamic')
     
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
