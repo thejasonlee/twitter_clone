3133eb5451f0e322fa1f0b62431ff2bd16103bca
Aaron Shyuu <aaronshyuu@gmail.com>
2020-11-19 15:20:58 -0800

    update model

--- project/__init__.py
+++ project/__init__.py
@@ -1,20 +1,20 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-#from sqlalchemy import MetaData
+from sqlalchemy import MetaData
 from flask_migrate import Migrate
 from flask_login import LoginManager, login_required, login_user, logout_user
 from flask_bootstrap import Bootstrap
 from flask_bcrypt import Bcrypt
 
 
-#naming_convention = {
- #   "ix": 'ix_%(column_0_label)s',
-  #  "uq": "uq_%(table_name)s_%(column_0_name)s",
-   # "ck": "ck_%(table_name)s_%(column_0_name)s",
-   # "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-   # "pk": "pk_%(table_name)s"
-#}
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
 
 
 app = Flask(__name__)
@@ -24,11 +24,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
         "DATABASE_URL", f"sqlite:///app.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-# ,metadata=MetaData(naming_convention=naming_convention)
+db = SQLAlchemy(app=app, metadata=MetaData(naming_convention=naming_convention))
 db.init_app(app)
-migrate = Migrate(app, db)
-# render_as_batch=True
+migrate = Migrate(app, db, render_as_batch=True)
 bootstrap = Bootstrap(app)
 login = LoginManager(app)
 bcrypt = Bcrypt(app)
--- project/models.py
+++ project/models.py
@@ -9,7 +9,7 @@ def load_user(id):
 
 
 #followers = db.Table(
- #   'followers',
+   # 'followers',
   #  db.Column('follower_id', db.Integer, db.ForeignKey('user.id'), nullable=True),
    # db.Column('followed_id', db.Integer, db.ForeignKey('user.id'), nullable=True)
 #)
@@ -20,7 +20,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-#    posts = db.relationship('Post', backref ='author', lazy='dynamic')
+    posts = db.relationship('Post', backref ='author', lazy='dynamic')
     
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
@@ -30,7 +30,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text)
     like_count = db.Column(db.Integer, default=0)
- #   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f"Post('id: {self.id}, content:{self.content}, likes:{self.like_count}')"
