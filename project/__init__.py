fd004cf0c13662779aef04c90a8910b87393975f
Aaron Shyuu <aaronshyuu@gmail.com>
2020-12-11 13:10:05 -0800

    add mail config and init

--- project/__init__.py
+++ project/__init__.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager, login_required, login_user, logout_user
 from flask_bootstrap import Bootstrap
 from flask_bcrypt import Bcrypt
-# from flask_mail import Mail
+from flask_mail import Mail
 
 
 naming_convention = {
@@ -25,12 +25,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
         "DATABASE_URL", f"sqlite:///app.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_SSL = True
+MAIL_USERNAME = os.environ.get('GMAIL_USERNAME') or 'MAIL_USERNAME'
+MAIL_PASSWORD = os.environ.get('GMAIL_PASSWORD') or 'MAIL_PASSWORD'
+
+
 db = SQLAlchemy(app=app, metadata=MetaData(naming_convention=naming_convention))
 db.init_app(app)
 migrate = Migrate(app, db, render_as_batch=True)
 bootstrap = Bootstrap(app)
+mail = Mail(app)
 login = LoginManager(app)
 bcrypt = Bcrypt(app)
-# mail = Mail(app)
+
 
 from project import routes
